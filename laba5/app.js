a389021bd3ac89304987ae42340c00da239e41b5
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2022-10-11 20:38:05 +0300

    Create app.js

---
+++ laba5/app.js
@@ -0,0 +1,19 @@
+function click1() {
+    let f1 = document.getElementById('field1').value;
+    let f2 = document.getElementById('field2').value;
+    let res = parseInt(f1)*parseInt(f2);
+        if ((isNaN(res)) || (res<0)) {
+           document.getElementById('result').innerHTML="Вы ввели некорректное значение :( Попробуйте снова";}
+           else
+    document.getElementById('result').innerHTML="Общая стоимость: "+res;
+}
+
+function onClick() {
+  alert("click");
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+  let b = document.getElementById("button1");
+  b.addEventListener("click", onClick);
+});
