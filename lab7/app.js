153f9803201ca751880c7e2093fc3d977302a185
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2022-10-26 17:46:27 +0300

    Create app.js

---
+++ lab7/app.js
@@ -0,0 +1,15 @@
+$('.single-item').slick({
+	infinite: false,
+	dots: true,
+	slidesToShow: 4,
+	slidesToScroll: 2,
+	responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        dots: false
+	      }
+	    }
+    ]
+});
