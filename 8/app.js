0a33f7550acea4e46730a57dee5d5ccbb72f28a0
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2022-11-18 19:53:52 +0300

    Update app.js

--- 8/app.js
+++ 8/app.js
@@ -1,12 +1,13 @@
-$('.window').click(function(e) {
+$(document).ready(function() {
+$(".window").click(function(e) {
     e.preventDefault();
-    $('.popup-bg').fadeIn(500);
-    $('html').addClass('no-scroll');
+    $(".popup-bg").fadeIn(500);
+    $("html").addClass("no-scroll");
 });
 
-$('.close-popup').click(function() {
-    $('.popup-bg').fadeOut(500);
-    $('html').removeClass('no-scroll');
+$(".close-popup").click(function() {
+    $(".popup-bg").fadeOut(500);
+    $("html").removeClass("no-scroll");
 });
 
 
@@ -28,4 +29,6 @@ $(function(){
             }
         });
     });
+});
+
 });
