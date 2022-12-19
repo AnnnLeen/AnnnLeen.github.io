9ee92a4b249703b3aab160003d101f308a9674c5
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2022-12-19 21:53:07 +0300

    Update main.js

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,68 @@
+$('.slick-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+$('.slick-slider-two').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    accessibility: false,
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                slidesToShow: 2
+            }
+        }
+    ]
+});
+$('.slick-slider-three').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    accessibility: false,
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                slidesToShow: 2
+            }
+        }
+    ]
+});
+
 $(function(){
     $(".ajaxForm").submit(function(e){
         e.preventDefault();
