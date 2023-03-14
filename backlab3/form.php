bfbb98673d51e0cf2d28068f968beef9e79825ff
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2023-03-14 19:37:57 +0300

    Update form.php

--- backlab3/form.php
+++ backlab3/form.php
@@ -1,33 +1,39 @@
 <form action="" method="POST">
   
-  <input name="fio">
+  <input name="fio" />
   
-  <select name="year">
+<select name="year">
      <?php 
     for ($i = 1922; $i <= 2022; $i++) {
       printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>   
+    } ?>
+</select>   
     
-  <input name="email">
+  <input name="email" />
   
-  <select name="gender">
-   </select>
+<select name="gender">
+    <?php 
+    for ($i = 0; $i <= 1; $i++) {
+      printf('<option value="%d">%d</option>', $i, $i);
+    } ?>
+ </select>
     
-  <select name="limbs">
+<select name="limbs">
   <?php 
     for ($i = 0; $i <= 4; $i++) {
       printf('<option value="%d">%d</option>', $i, $i);
     }
     ?>
-  </select>
+</select>
 
-  <input name="biography">
+  <input name="biography" />
     
-  <select name="checkbox">
-    
-
+<select name="checkbox">
+  <?php 
+    for ($i = 0; $i <= 1; $i++) {
+      printf('<option value="%d">%d</option>', $i, $i);
+    } ?>
+</select>
   
   <input type="submit" value="ok" />
 </form>
