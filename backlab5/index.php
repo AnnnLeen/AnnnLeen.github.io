edddec94a263ac0f1ec336c627749ef4a1367fff
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2023-04-30 13:54:42 +0300

    Update index.php

--- backlab5/index.php
+++ backlab5/index.php
@@ -265,7 +265,7 @@ try {
         foreach ($abilities as $ability) {
              $second_stmt -> execute([$app_id, $ability]); }
             
-        $third_stmt = $db->prepare("INSERT INTO login SET user_id = ?, login = ?, pwd = ?);
+        $third_stmt = $db->prepare("INSERT INTO login SET user_id = ?, login = ?, pwd = ?");
         $third_stmt->execute(array($id, $login, password_hash($pwd, PASSWORD_DEFAULT)));
       
 }
