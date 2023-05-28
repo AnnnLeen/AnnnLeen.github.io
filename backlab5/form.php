e7f3eba4679e1390af7c0144430a3735bf6632a9
AnnnLeen <112850413+AnnnLeen@users.noreply.github.com>
2023-05-28 23:10:19 +0300

    Update form.php

--- backlab5/form.php
+++ backlab5/form.php
@@ -32,39 +32,41 @@ if (!empty($messages)) {
 <label>
         <div class="txt">Имя:</div>
         <input name="fio" type="text" placeholder="Введите ваше имя"
-               <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
+               <?php if ($errors['fio_empty']) {print 'class="error"';} ?> value="<?php print $values['fio_value']; ?>" />
 </label>
 
 <label>
         <div class="txt">Email:</div>
         <input name="email" type="email" placeholder="Введите вашу почту"
-               <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" />
+               <?php if ($errors['email_empty']) {print 'class="error"';} ?> 
+               value="<?php print $values['email_value']; ?>" />
 </label>
 
 <label>
         <div class="txt">Год рождения:</div>
-        <select name="year"
-                <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" />
-  <?php 
-    for ($i = 1922; $i <= 2022; $i++) { 
-      printf('<option value="%d">%d год</option>', $i, $i);
-    } ?>
-        </select>
+        <select <?php if ($errors['year_empty']) {print 'class="form-select error"';}?> name="year" id="year">
+                    <option selected="selected" value="---">---</option>
+                    <?php for($i = 1900; $i < 2020; $i++) {?>
+                          <option <?php if($values['year_value'] == $i) print 'selected="selected"';?> value="<?php print $i; ?>"><?= $i; ?></option>
+                  <?php }?>
+                </select>
 </label>
 
       
 
 <label>
-      <div class="txt" <?php if ($errors['gender']) {print 'class="error"';}?>>Пол:</div>
+      <div class="txt" <?php if ($errors['gender_empty']) {print 'class="error"';}?>>Пол:</div>
   
       <label><div class="raddio">
-        <input type="radio" name="gender" id="gender1" value="0" <?php if($values['gender'] == 0) print 'checked';?>
+        <input type="radio" name="gender" id="gender1" value="0" 
+               <?php if($values['gender_value'] == 0) print 'checked';?>
                
                >Мужской<span></span>
         </div></label>
   
       <label><div class="raddio">
-        <input type="radio" name="gender" id="gender2" value="1" <?php if($values['gender'] == 1) print 'checked';?>
+        <input type="radio" name="gender" id="gender2" value="1" 
+               <?php if($values['gender_value'] == 1) print 'checked';?>
                >Женский<span></span>
         </div></label>
                                                               
@@ -76,47 +78,48 @@ if (!empty($messages)) {
       <div class="txt">Количество конечностей:</div>
       <label><div class="raddio">
            <input type="radio" name="limbs" id="limb1" value="1"
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> 
+                  value="<?php print $values['limbs_value']; ?>" />
         1<span></span></div></label>
   
       <label><div class="raddio">
            <input type="radio" name="limbs" id="limb2" value="2" 
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         2<span></span></div></label>
   
       <label><div class="raddio">
            <input type="radio" name="limbs" id="limb3" value="3" 
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         3<span></span></div></label>
   
         <label><div class="raddio">
   <input type="radio" name="limbs" id="limb4" value="4"
-         <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+         <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         4<span></span></div></label>
   
   <label><div class="raddio">
            <input type="radio" name="limbs" id="limb5" value="5"
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         5<span></span></div></label>
   
   <label><div class="raddio">
            <input type="radio" name="limbs" id="limb6" value="6" 
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         6<span></span></div></label>
   
   <label><div class="raddio">
            <input type="radio" name="limbs" id="limb7" value="7"
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         7<span></span></div></label>
   
       <label><div class="raddio">
            <input type="radio" name="limbs" id="limb8" value="8"
-                  <?php if ($errors['limbs']) {print 'class="error"';} ?> value="<?php print $values['limbs']; ?>" />
+                  <?php if ($errors['limbs_empty']) {print 'class="error"';} ?> value="<?php print $values['limbs_value']; ?>" />
         8<span></span></div></label>
 </label>
                                                                 
 <label>
-        <div class="txt" <?php if ($errors['abilities']) {print 'class="error"';} ?>>Сверхспособности:</div>
+        <div class="txt" <?php if ($errors['abilities_empty']) {print 'class="error"';} ?>>Сверхспособности:</div>
         <select class="form-select" name="abilities[]" multiple="multiple">
           <option <?php if($values['ability0']==1) print 'selected="selected"';?> value="1">Бессмертие</option>
           <option <?php if($values['ability1']==1) print 'selected="selected"';?> value="2">Прохождение сквозь стены</option>
@@ -129,12 +132,12 @@ if (!empty($messages)) {
         
 <label><div class="txt">Биография:</div>
         <textarea name="biography" placeholder="Напишите свою биографию"
-                  <?php if ($errors['biography']) {print 'class="error"';} ?> value="<?php print $values['biography']; ?>"></textarea>
+                  <?php if ($errors['biography_empty']) {print 'class="error"';} ?> value="<?php print $values['biography_value']; ?>"></textarea>
  </label>
                                                                         
  <label>
     <div class="check"><input type="checkbox" name="accept" value="1"
-                              <?php if ($errors['accept']) {print 'class="error"';} ?> value="<?php print $values['accept']; ?>" />
+                              <?php if ($errors['accept_empty']) {print 'class="error"';} ?> value="<?php print $values['accept_value']; ?>" />
         С контрактом ознакомлен(а)<span1></span1></div></label>
 
 <div class="otpr"><label><button type="submit" value="send">Отправить!</button></label></div>
